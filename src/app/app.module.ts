b1bbc0e1c1366492c59a304eee84dad315128c0e
jferco <jferco94@gmail.com>
2018-12-18 23:49:10 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ListComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  lista=[{avatar:"pablex.jpg",title:"Pablo Matamoros",subtitle:"Conocido como DJ"},
+  {avatar:"vitorsh.jpg",title:"Víctor Vargas",subtitle:"El famoso Don Vergas"}];
+  constructor() {}
+
+  doRefresh(event) {
+    console.log('Empieza a Refrescar');
+
+    setTimeout(() => {
+      console.log('Termina de refrescar');
+      event.target.complete();
+    }, 2000);
+  }
+
 }
